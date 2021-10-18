10fad9e967d422645e535ae42bbf139754276ef4
Harry Evans <henryjohnevans@icloud.com>
2021-10-17 20:37:25 -0400

    images deleted. Contacts note fading in so fixed this by placing a tags outside the fade tags.

--- portfolio-fe/src/components/Contact.js
+++ portfolio-fe/src/components/Contact.js
@@ -3,35 +3,34 @@ import Fade from 'react-reveal/Fade'
 export default function Contact() {
   return (
     <div className="container contacts-container">
-      
       <div className="contact-text">
         <p className="title">Contact Me</p>
-        <Fade duration={2000} >
-          <a
-            href="mailto:harry.evans9191.com"
-            subject="I was looking at your Portfolio...">
+        <a
+          href="mailto:harry.evans9191.com"
+          subject="I was looking at your Portfolio...">
+          <Fade duration={2000} >
             <div className="email" >
               <img alt="Email Me" title="Email Me" src="https://res.cloudinary.com/dn11uqgux/image/upload/v1634006167/project-setup-test/email_inbox_letter_send_icon_rctils.png" />
               <p className="subtitle">harry.evans9191@gmail.com</p>
             </div>
-          </a>
-        </Fade>
-        <Fade duration={2000} delay={300} >
-          <a target="_blank" href="https://www.linkedin.com/in/harryevans9191/" rel="noreferrer">
+          </Fade>
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/harryevans9191/" rel="noreferrer">
+          <Fade duration={2000} delay={300} >
             <div className="linkdin">
               <img alt="LinkedIn" title="LinkedIn" src="https://res.cloudinary.com/dn11uqgux/image/upload/v1634006167/project-setup-test/linkedin_network_social_network_linkedin_logo_icon_izevbl.png" />
               <p className="subtitle">linkedin.com/in/harryevans9191/</p>
             </div>
-          </a>
-        </Fade>
-        <Fade duration={2000} delay={600} >
-          <a target="_blank" href="https://github.com/hatch9191" rel="noreferrer">
+          </Fade>
+        </a>
+        <a target="_blank" href="https://github.com/hatch9191" rel="noreferrer">
+          <Fade duration={2000} delay={600} >
             <div className="github">
               <img alt="GitHub" title="GitHub" src="https://res.cloudinary.com/dn11uqgux/image/upload/v1634006167/project-setup-test/social_github_icon_kiz1ng.png" />
               <p className="subtitle">github.com/hatch9191</p>
             </div>
-          </a>
-        </Fade>
+          </Fade>
+        </a>
       </div>
     </div>
   )
